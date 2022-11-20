af93df66a02ff4592d671a037ddfab3b108a37b4
julianpodruhin <yulianpodruhin@mail.ru>
2022-11-21 01:44:43 +0300

    Task_2_2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        University univercity_1= new University("SPBPU","Polytecnicheskay 29",30000,273.2);
+        System.out.println(univercity_1);
+    }
+
+}
